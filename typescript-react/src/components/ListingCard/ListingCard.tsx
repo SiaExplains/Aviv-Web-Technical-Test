07c5d7ebd5736bb77c53077a69ac0991642696c8
Siavash Ghanbari <sia.qnbr@gmail.com>
2023-07-20 11:56:26 +0200

    FE: Show listings from real fetched data

--- typescript-react/src/components/ListingCard/ListingCard.tsx
+++ typescript-react/src/components/ListingCard/ListingCard.tsx
@@ -1,38 +1,70 @@
+import { Link } from 'react-router-dom';
+
 import styles from './listing-card.module.scss';
 
-const ListingCard = () => {
+import { ListingItem } from '@/types';
+
+type ListingCardProps = ListingItem;
+
+const ListingCard = ({
+  id,
+  created_date,
+  updated_date,
+  name,
+  price,
+  postal_address,
+  rooms_count,
+  bedrooms_count,
+  surface_area_m2,
+  description,
+  building_type,
+  contact_phone_number,
+}: ListingCardProps) => {
+  const { price_eur } = price;
+  const { postal_code, city, street_address } = postal_address;
+
   return (
     <article className={styles['listing-card']}>
-      <span className={styles['listing-card__price']}>320 000 &euro;</span>
+      <h1>{name}</h1>
+      <span className={styles['listing-card__price']}>{price_eur} &euro;</span>
       <ul className={styles['listing-card__properties']}>
-        <li className={styles['listing-card__properties-item']}>Studio</li>
         <li className={styles['listing-card__properties-item']}>
-          74m<sup>2</sup>
+          {building_type}
         </li>
-        <li className={styles['listing-card__properties-item']}>3 rooms</li>
+        <li className={styles['listing-card__properties-item']}>
+          {surface_area_m2}m<sup>2</sup>
+        </li>
+        <li className={styles['listing-card__properties-item']}>
+          {rooms_count} rooms
+        </li>
+        <li className={styles['listing-card__properties-item']}>
+          {bedrooms_count} Bedroom
+        </li>
       </ul>
       <section className={styles['listing-card__address']}>
-        <address>48, boulevard des capucins, 10294, Paris</address>
+        <address>
+          {street_address}, {postal_code}, {city}
+        </address>
       </section>
       <section className={styles['listing-card__description']}>
         <h3>Property description: </h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          commodo, arcu eu varius dapibus, lacus velit posuere tellus, nec
-          convallis sem velit ut leo. Maecenas maximus volutpat felis.
-        </p>
+        <p>{description}</p>
       </section>
       <div className={styles['listing-card__footer']}>
         <p className={styles['listing-card__reference']}>
-          Ref: 123456 <br />
-          Last update: 2021/12/31
+          Ref: {id} <br />
+          Created at: {new Date(created_date).toLocaleDateString('en-US')}
+          <br />
+          Last update: {new Date(updated_date).toLocaleDateString('en-US')}
         </p>
-        <a href="/" className={styles['listing-card__link']}>
+        <Link to={`/${id}/prices`} className={styles['listing-card__link']}>
           See history &rarr;
-        </a>
+        </Link>
       </div>
     </article>
   );
 };
 
 export default ListingCard;
+
+export type { ListingCardProps };
--- typescript-react/src/components/ListingForm/ListingForm.tsx
+++ typescript-react/src/components/ListingForm/ListingForm.tsx
@@ -1,55 +1,48 @@
+import { useEffect, useState } from 'react';
+import ListingCard from '@components/ListingCard';
+import ListingForm from '@components/ListingForm';
+
 import styles from './listing-form.module.scss';
 
-const ListingForm = () => {
-  const submit = () => null;
+import { fetchListings } from '@/services/listings.service';
+import { ListingItem } from '@/types';
+
+const Listings = () => {
+  const [listings, setListings] = useState([]);
+  const hasListingData = listings && listings.length > 0;
+
+  useEffect(() => {
+    const fetchListingsData = async () => {
+      const data = await fetchListings();
+      setListings(data);
+    };
+
+    fetchListingsData();
+  }, []);
 
   return (
-    <form className={styles['listing-form']} onSubmit={submit}>
-      <div className={styles['listing-form__card']}>
-        <div className={styles['listing-form__input-group']}>
-          <label htmlFor="latest_price_eur">Price:</label>
-          <input
-            type="text"
-            name="latest_price_eur"
-            className={styles['listing-form__input-text']}
-          />
-        </div>
-        <div className={styles['listing-form__input-group']}>
-          <label htmlFor="surface_area_m2">Area:</label>
-          <input
-            type="text"
-            name="surface_area_m2"
-            className={styles['listing-form__input-text']}
-          />
-        </div>
-        <div className={styles['listing-form__input-group']}>
-          <label htmlFor="street_address">Street address:</label>
-          <input
-            type="text"
-            name="street_address"
-            className={styles['listing-form__input-text']}
-          />
-        </div>
-        <div className={styles['listing-form__input-group']}>
-          <label htmlFor="building_type">Building type:</label>
-          <select
-            name="building_type"
-            className={styles['listing-form__select']}
-          >
-            <option value="STUDIO">Studio</option>
-            <option value="APARTMENT">Apartment</option>
-            <option value="HOUSE">House</option>
-          </select>
-        </div>
-        <button
-          type="submit"
-          className={styles['listing-form__button--submit']}
-        >
-          Create
-        </button>
+    <main className={styles['listings']}>
+      <h1 className={styles['listings__title']}>Main Listings page</h1>
+      <div className={styles['listings__wrapper']}>
+        <aside className={styles['listings__aside']}>
+          <h2 className={styles['listings__sub-title']}>Add a listing</h2>
+          <ListingForm />
+        </aside>
+        <section className={styles['listings__section']}>
+          <h2 className={styles['listings__sub-title']}>Listings</h2>
+          {hasListingData &&
+            listings.map((prop: ListingItem) => (
+              <ListingCard key={prop.id} {...prop} />
+            ))}
+          {!hasListingData && (
+            <>
+              There is no listing added yet. please use the form to create one
+            </>
+          )}
+        </section>
       </div>
-    </form>
+    </main>
   );
 };
 
-export default ListingForm;
+export default Listings;
