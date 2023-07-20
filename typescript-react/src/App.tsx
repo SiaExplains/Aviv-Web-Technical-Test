07e8f9d338d11e0c1a7dd1ebdb8fac65a37011ea
Siavash Ghanbari <sia.qnbr@gmail.com>
2023-07-20 11:38:39 +0200

    FE: Add ErrorBoundary to cover all exceptions

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -2,15 +2,17 @@ import Header from '@components/Header/Header';
 import Listings from '@containers/Listings/Listings';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import ErrorBoundary from './ErrorBoundary';
+
 const App = () => (
-  <>
+  <ErrorBoundary>
     <Header />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Listings />} />
       </Routes>
     </BrowserRouter>
-  </>
+  </ErrorBoundary>
 );
 
 export default App;
