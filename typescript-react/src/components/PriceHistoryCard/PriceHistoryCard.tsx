6b6e45a6a55e8e2bc84a5e374b8088eb97b2a81e
Siavash Ghanbari <sia.qnbr@gmail.com>
2023-07-20 12:12:58 +0200

    FE: Fetch data and show PriceHistory

--- typescript-react/src/components/PriceHistoryCard/PriceHistoryCard.tsx
+++ typescript-react/src/components/PriceHistoryCard/PriceHistoryCard.tsx
@@ -1,6 +1,13 @@
 import styles from './price-history-card.module.scss';
 
-const PriceHistoryCard = () => {
+import { PriceInfo } from '@/types';
+
+export type PriceHistoryCardProps = {
+  prices: PriceInfo[] | [];
+};
+
+const PriceHistoryCard = ({ prices }: PriceHistoryCardProps) => {
+  const hasPriceData = prices && prices.length > 0;
   return (
     <div className={styles['container']}>
       <table className={styles['price-card']}>
@@ -9,10 +16,15 @@ const PriceHistoryCard = () => {
             <th scope="col">Date</th>
             <th scope="col">Price (eur)</th>
           </tr>
-          <tr>
-            <td>01/01/2022</td>
-            <td>500 000</td>
-          </tr>
+          {hasPriceData &&
+            prices.map((price, index) => (
+              <tr key={index}>
+                <td>
+                  {new Date(price.created_date).toLocaleDateString('en-US')}
+                </td>
+                <td>{price.price_eur}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
--- typescript-react/src/containers/PricesHistory/PricesHistory.tsx
+++ typescript-react/src/containers/PricesHistory/PricesHistory.tsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from 'react';
 import PricesHistoryCard from '@components/PriceHistoryCard';
+import { Link, redirect, useLocation } from 'react-router-dom';
 
 import styles from './prices-history.module.scss';
 
+import { fetchAllPriceHistory } from '@/services/listings.service';
+
 const PricesHistory = () => {
+  const [prices, setPrices] = useState([]);
+  const hasPriceHistory = prices && prices.length > 0;
+  const location = useLocation();
+
+  useEffect(() => {
+    const fetchListingsData = async () => {
+      const pathSlugs = location.pathname.split('/');
+      if (pathSlugs.length < 3) {
+        redirect('/404');
+      }
+      const [, id] = pathSlugs;
+      const data = await fetchAllPriceHistory(id);
+      setPrices(data);
+    };
+
+    fetchListingsData();
+  }, []);
+
   return (
     <div className={styles['container']}>
       <h1>Prices History</h1>
-      <PricesHistoryCard />
+      {hasPriceHistory && <PricesHistoryCard prices={prices} />}
+      {!hasPriceHistory && <p>There is no price added at the moment!</p>}
 
-      <a href="/" className={styles['link']}>
+      <Link to="/" className={styles['link']}>
         &larr; Back Home
-      </a>
+      </Link>
     </div>
   );
 };
